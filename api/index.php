1deaec414abe3feec3cdfd96b39b4acd81959e06
luongthedung2003 <luongthedung2001@gmail.com>
2026-05-14 22:15:28 +0700

    Force Laravel to use /tmp for storage and cache on Vercel

--- api/index.php
+++ api/index.php
@@ -1,11 +1,16 @@
 <?php
 
 // Vercel is read-only, so we must use /tmp for compilation and logs
-putenv('VIEW_COMPILED_PATH=/tmp');
+if (isset($_SERVER['VERCEL'])) {
+    mkdir('/tmp/storage/framework/views', 0755, true);
+    mkdir('/tmp/storage/framework/sessions', 0755, true);
+    mkdir('/tmp/storage/framework/cache', 0755, true);
+    mkdir('/tmp/storage/logs', 0755, true);
+}
+
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
 putenv('SESSION_DRIVER=cookie');
 putenv('LOG_CHANNEL=stderr');
-putenv('APP_CONFIG_CACHE=/tmp/config.php');
-putenv('APP_ROUTES_CACHE=/tmp/routes.php');
 
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,10 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+if (isset($_SERVER['VERCEL'])) {
+    $app->useStoragePath('/tmp/storage');
+}
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
