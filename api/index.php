4b5020ae01f250db1bcc911a8d6f5efb95d01775
luongthedung2003 <luongthedung2001@gmail.com>
2026-05-14 22:13:15 +0700

    Configure Laravel for read-only filesystem

--- api/index.php
+++ api/index.php
@@ -1,5 +1,12 @@
 <?php
 
+// Vercel is read-only, so we must use /tmp for compilation and logs
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('SESSION_DRIVER=cookie');
+putenv('LOG_CHANNEL=stderr');
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
