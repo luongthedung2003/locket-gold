3b5732fe170915d11854e1b483e4d474b0320014
luongthedung2003 <luongthedung2001@gmail.com>
2026-05-14 21:40:57 +0700

    Diagnostic build with vercel-php@0.7.5

--- api/index.php
+++ api/index.php
@@ -1,4 +1,13 @@
 <?php
+$file = __DIR__ . '/../public/index.php';
 
-// Forward Vercel requests to normal Laravel public/index.php
-require __DIR__ . '/../public/index.php';
+if (file_exists($file)) {
+    require $file;
+} else {
+    echo "<h1>Debug Info</h1>";
+    echo "<b>Error:</b> public/index.php not found!<br>";
+    echo "<b>Current Path:</b> " . __DIR__ . "<br>";
+    echo "<b>Looking for:</b> " . realpath($file) . "<br>";
+    echo "<b>Files in parent dir:</b><br>";
+    print_r(scandir(__DIR__ . '/..'));
+}
